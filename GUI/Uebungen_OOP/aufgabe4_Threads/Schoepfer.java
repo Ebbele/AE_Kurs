f0596a4d6927e3d1337adb29e995cea04a755f3a
Tobias Ebbers <tobias@ebbers.biz>
2017-06-23 11:57:08 +0200

    angepasst an aufgabenstellung

--- GUI/Uebungen_OOP/aufgabe4_Threads/Schoepfer.java
+++ GUI/Uebungen_OOP/aufgabe4_Threads/Schoepfer.java
@@ -1,15 +1,22 @@
 package aufgabe4_Threads;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Schoepfer {
+public class Schoepfer extends Thread {
 
 	private static final Lock lock = new ReentrantLock();
 	private static final Condition condFill = lock.newCondition();
 
 	private static int wassertrog = 0;
+
+	private int state = -1;
+
+	public Schoepfer(int state) {
+		this.state = state;
+	}
 
 	private static int randomInt() {
 		return (int) (Math.random() * 20.0);
@@ -17,6 +24,26 @@ public class Schoepfer {
 
 	public void getTrogStand(String what, int c) {
 		System.out.println(what + " " + c + "\tF\u00fcllstand: " + wassertrog);
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				switch (state) {
+				case 0:
+					fill();
+					break;
+				case 1:
+					remove();
+					break;
+
+				}
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public void fill() {
@@ -36,7 +63,7 @@ public class Schoepfer {
 			int remove = randomInt();
 			while (wassertrog < remove) {
 				try {
-					System.out.println(remove + " nicht entnehmbar F\u00fcllstand: "+ wassertrog);
+					System.out.println(remove + " nicht entnehmbar F\u00fcllstand: " + wassertrog);
 					condFill.await();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
--- GUI/Uebungen_OOP/aufgabe4_Threads/Ziehbrunnen.java
+++ GUI/Uebungen_OOP/aufgabe4_Threads/Ziehbrunnen.java
@@ -1,41 +1,15 @@
 package aufgabe4_Threads;
 
-import java.util.concurrent.TimeUnit;
-
 public class Ziehbrunnen {
 
 	public static void main(String[] args) {
-		Schoepfer schoepfer = new Schoepfer();
 		System.out.println("Starte Ziehbrunnen");
-		Thread pumpe = new Thread() {
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						schoepfer.fill();
-						TimeUnit.SECONDS.sleep(1);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		};
-		pumpe.start();
 
-		Thread verteiler = new Thread() {
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						schoepfer.remove();
-						TimeUnit.SECONDS.sleep(1);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		};
+		Schoepfer pumpe = new Schoepfer(0);
+		Schoepfer verteiler = new Schoepfer(1);
+		pumpe.start();
 		verteiler.start();
+		
 	}
 
 }
