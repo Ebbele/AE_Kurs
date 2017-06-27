5930af17be354e2d558ce023e1cc09704c04334f
Ivo <Ivo@HAL9000.fritz.box>
2017-06-27 16:17:33 +0200

    Zahlenteiler200 Superdeluxe

--- Pizza_JE_BK/src/Dateien_JE_BK/Division.java
+++ Pizza_JE_BK/src/Dateien_JE_BK/Division.java
@@ -2,13 +2,12 @@ package Dateien_JE_BK;
 
 import java.awt.Container;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.text.DecimalFormat;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Division {
@@ -21,41 +20,64 @@ public class Division {
 
 	
 	Division() {
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("Zahlenteiler2000");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container container = new Container();
 		container = frame.getContentPane();
-		container.setLayout(new GridLayout(2,2));
+		container.setLayout(new GridLayout(3,2));
 		
 		JLabel ldividend = new JLabel("dividend");
 		container.add(ldividend);
 		JTextField txtdividend = new JTextField();
+		
 		container.add(txtdividend);
 		
 		JLabel ldivisor = new JLabel("divisor");
 		container.add(ldivisor);
 		JTextField txtdivisor = new JTextField();
+		container.add(txtdivisor);
+		JLabel txtErgebnis = new JLabel("");
+		container.add(txtErgebnis);
+		
+		
+		
+		//Rechnung bei Return ausführen und zurücksetzen
 		txtdivisor.addKeyListener(new KeyAdapter(){
 			public void keyPressed(KeyEvent ae) {
 				if (ae.getKeyCode()==KeyEvent.VK_ENTER) {
-					integerDivision(txtdividend.getText(),txtdivisor.getText());
+					txtErgebnis.setText(integerDivision(txtdividend.getText(),txtdivisor.getText()));
+					
+					
+					txtdividend.requestFocusInWindow();
+					txtdivisor.setText("");
+					txtdividend.setText("");
 					
 					
 				}
 			}
 		});
-		container.add(txtdivisor);
 		
 		
+		//Cursor Zurücksetzen
+		txtdividend.addKeyListener(new KeyAdapter(){
+			public void keyPressed(KeyEvent ae) {
+				if (ae.getKeyCode()==KeyEvent.VK_ENTER) {
+					txtdivisor.requestFocusInWindow();
+					
+				}
+			}
+		});
+	
 		
 		
-		frame.setSize(200, 100);
+		frame.setSize(400, 100);
 		frame.setVisible(true);
 		
 	}
 	
-	public void integerDivision(String dividend, String divisor) {
-		
+	public String integerDivision(String dividend, String divisor) {
+		double ergebnis=0;
+		String rueck = new String("");
 		try {
 			if (dividend.equals("")||divisor.equals("")) {
 				throw new IllegalArgumentException();
@@ -66,16 +88,23 @@ public class Division {
 			if (divisor.equals("0")) {
 				throw new ArithmeticException();
 			}
-			double ergebnis = Double.parseDouble(dividend)/Double.parseDouble(divisor);
-			System.out.printf("%.2f\n",ergebnis);
+			ergebnis = Double.parseDouble(dividend)/Double.parseDouble(divisor);
+			DecimalFormat df = new DecimalFormat("0.00");
+			rueck = dividend+" : "+divisor+" = "+String.valueOf(df.format(ergebnis));
+			System.out.println(ergebnis);
+			
 		
 		} catch (NumberFormatException f) {
+			rueck = "Keine Zahl!";
 			System.out.println("Darf nur Zahlen enthalten");	
 		} catch (IllegalArgumentException e) {
+			rueck = "Keine Eingabe!";
 			System.out.println("Darf keinen Leerstring enthalten");
 		} catch (ArithmeticException g) {
+			rueck = "Durch Null geteilt!";
 			System.out.println("durch nurr geteilt du Idiot");
 		}
+		return rueck;
 		
 		
 	}
