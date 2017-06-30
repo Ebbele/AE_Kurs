7e3a412d7c66fa66974d5748bb41aa0c346e1781
jerbsloeh <jerbsloeh@PC01100206.cc-college.gecs.loc>
2017-06-30 12:38:58 +0200

    tzujzt

--- Pizza_JE_BK/src/Candy/Spielfeld.java
+++ Pizza_JE_BK/src/Candy/Spielfeld.java
@@ -6,19 +6,55 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Spielfeld {
 
+	
+	
+	
 	public void FeldGenerieren(int felder) {
+		
+		//Array mit Zahlenwerten für Farben
+		int[][] feld = new int[felder][felder];
+		
+		for (int i=0;i<feld.length;i++) {
+			for (int j=0;j<feld.length;j++) {
+				
+				
+				try {
+					do {
+						feld[i][j] = (int)((Math.random()) * 7+1);
+					} while ((feld[i][j]==feld[i][j-1] && feld[i][j]==feld[i][j-2])
+							|| (feld[i][j]==feld[i-1][j] && feld[i][j]==feld[i-2][j]) );
+				} catch (ArrayIndexOutOfBoundsException e1) {
+					// TODO Auto-generated catch block
+					System.out.println("ungültig");
+				}
+				
+				System.out.println(feld[i][j]);
+				
+				
+			}
+		}
+		
+		// Fenster Erstellen
 		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		Container container = new Container();
 		container = frame.getContentPane();
+		
+		// Grid mit übergebener Größe erstellen
+		
 		container.setLayout(new GridLayout(felder,felder));
 		ActionListener al = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -30,20 +66,24 @@ public class Spielfeld {
 			}
 		};
 		
-		for (int i=1;i<=felder;i++) {
-			for (int j=1;j<=felder;j++) {
-				//System.out.println(i+" "+j);
-				int zahl = (int)((Math.random()) * 7+1);
-				System.out.println(zahl);
+		//Buttons erstellen und Farbe anhand des Arrays wählen
+		for (int i=0;i<feld.length;i++) {
+			for (int j=0;j<feld.length;j++) {
 
+				
+				container.add(new createButton(feld[i][j]));
+				frame.pack();
+				
+				/*
 				JButton a1 = new JButton();
 				a1.addActionListener(al);
-				switch (zahl) {
+				
+				switch (feld[i][j]) {
 				case 1: a1.setBackground(Color.red); break;
 				case 2: a1.setBackground(Color.green); break;
 				case 3: a1.setBackground(Color.yellow); break;
 				case 4: a1.setBackground(Color.blue); break;
-				case 5: a1.setBackground(Color.white); break;
+				case 5: a1.setBackground(Color.cyan); break;
 				case 6: a1.setBackground(Color.orange); break;
 				case 7: a1.setBackground(Color.magenta); break;
 				
@@ -52,33 +92,60 @@ public class Spielfeld {
 				a1.setOpaque(true);
 				a1.setBorderPainted(false);
 				
+				
 				container.add(a1);
+				*/
 			}
+			
 			
 			
 			
 			
 		}
 		
-		/*
-		JButton a1 = new JButton();
-		a1.setBackground(Color.red);
-		a1.setOpaque(true);
-		a1.setBorderPainted(false);
-		a1.setSize(50, 50);
-		container.add(a1);
 		
-		JButton a2 = new JButton();
-		a2.setBackground(Color.green);
-		a2.setOpaque(true);
-		a2.setBorderPainted(false);
-		a2.setSize(50, 50);
-		container.add(a2);
-		*/
 		
 		//frame.setSize(felder*60, felder*60);
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	private class createButton extends JPanel {
+		
+		createButton me;
+		
+		public createButton(int color) {
+            
+			me = this;
+			
+			
+			
+            JButton button = new JButton();
+            switch (color) {
+			case 1: button.setBackground(Color.red); break;
+			case 2: button.setBackground(Color.green); break;
+			case 3: button.setBackground(Color.yellow); break;
+			case 4: button.setBackground(Color.blue); break;
+			case 5: button.setBackground(Color.cyan); break;
+			case 6: button.setBackground(Color.orange); break;
+			case 7: button.setBackground(Color.magenta); break;
+			
+			}
+            button.setPreferredSize(new Dimension (60,60));
+			button.setOpaque(true);
+			button.setBorderPainted(false);
+            
+            button.addActionListener(new ActionListener(){
+            
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                   System.out.println(me.getParent());
+                 
+                }
+            });
+            add(button);
+        }
+		
 	}
 	
 }
